621557f9d98562106a01db390f9d7fda4722c1f7
Rasheednour <rasheednour@gmail.com>
2022-12-02 17:43:31 +0200

    Set-up a simple express server

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+const express = require('express');
+const app = express();
+const { Datastore } = require('@google-cloud/datastore');
+const bodyParser = require('body-parser');
+const datastore = new Datastore();
+
+const router = express.Router();
+
+const USERS = "Users";
+
+app.use(bodyParser.json());
+app.enable('trust proxy');
+
+
+function fromDatastore(item) {
+    item.id = item[Datastore.KEY].id;
+    return item;
+}
+
+
+/* -------------Home Page Controller Function ------------- */
+
+
+router.get('/', function (req, res) {
+    res.send("Welcome to Posts-API");
+ });
+
+/* -------------Start Server ------------- */
+
+
+app.use('/', router);
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
